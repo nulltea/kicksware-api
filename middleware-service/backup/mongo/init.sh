9d30022395ec3c2ecf41db1c02edd842b90ad776
Timothy <admin@example.com>
2020-06-05 12:28:03 +0000

    Update middleware-service/docker-compose.yml, middleware-service/backup/mongo/init.sh files

--- middleware-service/backup/mongo/init.sh
+++ middleware-service/backup/mongo/init.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
 mongorestore --db=sneakerResaleDB  ./backup/mongo/sneakerResaleDB
+
+mongo admin -u admin -p admin --eval "db.getSiblingDB('dummydb').createUser({user: 'root', pwd: 'greenJordans', roles: ['readWrite']})"
