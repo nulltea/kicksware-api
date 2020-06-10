87dcef78de267a388d9387f607624763624e514c
Timothy <tyalugin@outlook.com>
2020-06-10 05:45:48 +0300

    fix (middleware) disk repository remove from ignore

---
+++ middleware-service/cdn-service/usecase/storage/disk/repository.go
@@ -0,0 +1,49 @@
+package disk
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/golang/glog"
+
+	"cdn-service/core/repo"
+	"cdn-service/env"
+)
+
+type repository struct {
+	storagePath string
+}
+
+func NewRepository(config env.DataStoreConfig) (repo.ContentRepository, error) {
+	path := config.URL
+	if _, err := os.Stat(path); err != nil && !os.IsExist(err) {
+		glog.Fatalln(err)
+		return nil, err
+	}
+
+	return &repository{path}, nil
+}
+
+
+func (r *repository) Download(from string, filename string) ([]byte, error) {
+	file, err := ioutil.ReadFile(r.filenameOf(from, filename))
+	if err != nil {
+		glog.Errorln(err)
+		return nil, err
+	}
+
+	return file, nil
+}
+
+func (r *repository) Upload(to string, filename string, content []byte) (string, error) {
+	err := ioutil.WriteFile(r.filenameOf(to, filename), content, 0600); if err != nil {
+		glog.Errorln(err)
+		return "", err
+	}
+	return filename, nil
+}
+
+func (r *repository) filenameOf(collection, filename string) string {
+	return fmt.Sprintf("%v/%v/%v", r.storagePath, collection, filename)
+}
