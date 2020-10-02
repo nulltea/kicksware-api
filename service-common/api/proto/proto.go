898528d3d7e9fca4624fffbc4e22617ce4c3a663
Timothy <timauthx@gmail.com>
2020-10-02 07:12:40 +0300

    ref (common) proto file go package import updated

--- service-common/api/proto/proto.go
+++ service-common/api/proto/proto.go
@@ -1,3 +1,3 @@
 package proto
 
-//go:generate protoc --proto_path=../../../service-protos  --go_out=plugins=grpc:./. common.proto
+//go:generate protoc --proto_path=../../../service-protos  --go_out=plugins=grpc,paths=source_relative:. common.proto
